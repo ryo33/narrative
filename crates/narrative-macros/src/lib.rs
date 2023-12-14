d0a0cdb06deb8ee29554c6e6f54e7c1577e3be19
Ryo Hirayama <ryo12redstone@gmail.com>
2023-12-14 13:53:14 +0900

    Add step macro for better error message

--- crates/narrative-macros/src/lib.rs
+++ crates/narrative-macros/src/lib.rs
@@ -2,9 +2,21 @@ mod story;
 mod story_syntax;
 
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{quote, quote_spanned};
 use story_syntax::ItemStory;
 use syn::parse_macro_input;
+
+/// This is not a real attribute macro, and it's just for changing the error message to be more
+/// friendly one.
+#[proc_macro_attribute]
+pub fn step(
+    attr: proc_macro::TokenStream,
+    _input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let span = attr.into_iter().next().expect("step attr").span().into();
+    quote_spanned! {span => compile_error!("step attribute must be used inside a story that is annotated with #[narrative::story]")}
+        .into()
+}
 
 #[proc_macro_attribute]
 pub fn story(
