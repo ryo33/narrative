a17e12f56b995fd508c9e3f25dca95f57f7f8bb2
Ryo Hirayama <ryo12redstone@gmail.com>
2025-04-30 15:54:57 +0900

    Modify consts example

--- examples/src/consts.rs
+++ examples/src/consts.rs
@@ -7,6 +7,9 @@ trait Consts {
 
     #[step("use const in format!", url = format!("https://example.com/{ID}"))]
     fn format_step(url: String);
+
+    #[step("use const in step text name: {NAME}")]
+    fn format_step_in_step_text();
 }
 
 #[test]
@@ -18,7 +21,7 @@ fn accessible_through_context() {
     assert_eq!(ConstsContext::NAME, "Ryo");
 
     let steps = ConstsContext.steps().collect::<Vec<_>>();
-    assert_eq!(steps.len(), 2);
+    assert_eq!(steps.len(), 3);
     assert_eq!(steps[0].args().count(), 1);
     let arg = steps[0].args().next().unwrap();
     assert_eq!(arg.debug_value(), r#""Ryo""#);
@@ -26,6 +29,7 @@ fn accessible_through_context() {
         serde_json::to_value(arg.serialize_value()).unwrap(),
         json!("Ryo")
     );
+
     assert_eq!(steps[1].args().count(), 1);
     let arg = steps[1].args().next().unwrap();
     assert_eq!(arg.debug_value(), r#""https://example.com/ryo33""#);
@@ -33,4 +37,7 @@ fn accessible_through_context() {
         serde_json::to_value(arg.serialize_value()).unwrap(),
         json!("https://example.com/ryo33")
     );
+
+    assert_eq!(steps[2].args().count(), 0);
+    assert_eq!(steps[2].step_text(), "use const in step text name: Ryo");
 }
